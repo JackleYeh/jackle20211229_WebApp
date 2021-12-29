f8972605205b5af0ba71e0017cb1a925122c7700
jackle yeh <jackle_yeh@phison.com>
2021-12-29 14:28:50 +0800

    20211229_1

---
+++ jackle20211229_WebApp/Controllers/LineChatController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace jackle20211229_WebApp.Controllers
+{
+    public class LineChatController : ApiController
+    {
+        public List<int> Get()
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        }
+    }
+}
---
+++ jackle20211229_WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jackle20211229_WebApp.WebApiApplication" Language="C#" %>
